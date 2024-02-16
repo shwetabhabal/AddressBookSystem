95f6260ac4b843a523202b3b10093616ee7c6b75
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-02-16 18:13:15 +0530

    add multiple person to Address Book

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class AddressBookSystem {
-    static Contact contacts[]= new Contact[100];
+    static Contact[] contacts = new Contact[100];
     static int count=0;
     public static Contact addcontact(){
         Scanner sc = new Scanner(System.in);
@@ -16,6 +16,7 @@ public class AddressBookSystem {
         contact.setCity(sc.next());
         System.out.println("Enter phone");
         contact.setPhone(sc.nextInt());
+        contacts[count++] = contact;
         return contact;
     }
     public static void editcontact(String firstname){
@@ -42,7 +43,7 @@ public class AddressBookSystem {
     public static void deletecontact(String firstname){
         boolean found =false;
         for(int i=0; i<count;i++){
-            if(contacts[i].getFirstname()==firstname){
+            if(Objects.equals(contacts[i].getFirstname(), firstname)){
                 for(int j=i; j<count-1; j++){
                     contacts[j] = contacts[j+1];
                 }
@@ -56,19 +57,54 @@ public class AddressBookSystem {
             System.out.println("Contact not found.");
         }
     }
+    public static void viewAllContacts() {
+        if (count == 0) {
+            System.out.println("Address book is empty.");
+        } else {
+            System.out.println("\nAll Contacts in the Address Book:");
+            for (int i = 0; i < count; i++) {
+                System.out.println(contacts[i]);
+                System.out.println("-----------------------------");
+            }
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Address Book");
-        contacts[count++] = addcontact();
-        System.out.println("enter name you want to edit: ");
-        String editname= sc.next();
-        editcontact(editname);
-        System.out.println("All cantacts:");
-        for (int i=0; i<count;i++){
-            System.out.println(contacts[i]);
+        while (true) {
+            System.out.println("\nAddress Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Edit Contact");
+            System.out.println("3. Delete Contact");
+            System.out.println("4. View All Contacts");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();  // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Adding new contact:");
+                    addcontact();
+                    break;
+                case 2:
+                    System.out.print("Enter the first name of the contact you want to edit: ");
+                    String editName = sc.nextLine();
+                    editcontact(editName);
+                    break;
+                case 3:
+                    System.out.print("Enter the first name of the contact you want to delete: ");
+                    String deleteName = sc.nextLine();
+                    deletecontact(deleteName);
+                    break;
+                case 4:
+                    viewAllContacts();
+                    break;
+                case 5:
+                    System.out.println("Exiting program...");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
         }
-        System.out.println("Enter name you want to delete: ");
-        String deletename= sc.next();
-        deletecontact(deletename);
     }
 }

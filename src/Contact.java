fad461323bd67995c7be9472d827a36a8f2c80aa
Shweta Bhabal <bhabalshweta02@gmail.com>
2024-02-16 17:38:57 +0530

    create a Contacts in Address Book

---
+++ src/Contact.java
@@ -0,0 +1,61 @@
+public class Contact {
+    String firstname;
+    String lastname;
+    String address;
+    String city;
+    int phone;
+
+    public Contact() {
+    }
+
+    public Contact(String firstname, String lastname, String address, String city, int phone) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.phone = phone;
+    }
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phone, String email) {
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+}
